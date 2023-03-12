37bbb25c3cef20c61d9843000b1ca71a36edf035
Kevin Dickerson <nolekev1214@gmail.com>
2023-03-12 12:51:31 -0400

    Fix incorrect path

--- omni_Power_Armor_19.mk
+++ omni_Power_Armor_19.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Power_Armor_19 device
 $(call inherit-product, device/ulefone/Power_Armor_19/device.mk)
